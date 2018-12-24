8d6f9c1e98e31d1e9cd46d91883c725abd671235
kitte anderson <kitteanderson@gmail.com>
2018-12-24 22:57:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,7 +18,9 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-    array.sort{|a, b| a.length <=> b.length}
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
 end
 
 
